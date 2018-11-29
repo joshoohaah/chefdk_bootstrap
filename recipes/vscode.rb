ddceb71ff56d456fc3ec8bf2ea53889a409ececb
joshschneider <joshoohaah@users.noreply.github.com>
2018-11-29 15:24:43 -0600

    testing vscode

--- recipes/vscode.rb
+++ recipes/vscode.rb
@@ -1,1 +1,14 @@
-chocolatey_package 'vscode'
+
+chocolatey_package 'vscode' do
+    action :install
+end
+
+case node['platform_family']
+when 'windows'
+  chocolatey_package 'vscode' do
+    action :install
+  end
+  # chocolatey_package 'poshgit'
+when 'mac_os_x'
+  package 'git'
+end
--- recipes/windows.rb
+++ recipes/windows.rb
@@ -30,4 +30,5 @@ packages.each do |pkg, install|
   include_recipe "#{cookbook_name}::#{pkg}" if install
 end
 
+include_recipe "#{cookbook_name}::vscode"
 include_recipe "#{cookbook_name}::powershell_profile" if node['chefdk_bootstrap']['powershell']['configure']
